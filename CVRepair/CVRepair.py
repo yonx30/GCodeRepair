1a05a305525b8e1df0532c983c15fb529a4dd338
Wu Yongxin <yonx30@gmail.com>
2023-10-29 12:01:07 -0700

    Final Commit for Calhacks

--- CVRepair/CVRepair.py
+++ CVRepair/CVRepair.py
@@ -6,20 +6,33 @@ import time
 import sys, asyncio
 import numpy as np
 import trimesh
-import os
+# import os
 
 sys.path.insert(0, r'C:\Users\yonx3\Documents\NUS Y2S1\NOC\Misc\Calhacks')
-path = r'C:\Users\yonx3\Documents\Crossbow\3DBenchy.stl'
-# path = r"C:\Users\yonx3\Documents\Crossbow\Random\T51_low_poly.stl"
+
+referenceModelPath = r'C:\Users\yonx3\Documents\Crossbow\3DBenchy.stl'
+# referenceModelPath = r"C:\Users\yonx3\Documents\Crossbow\Random\catstretch_voronoi updated.stl"
+
 savePath = r'C:\Users\yonx3\Documents\NUS Y2S1\NOC\Misc\Calhacks\Webcam Images\referenceSlice.jpg'
 meshPath = r'C:\Users\yonx3\Documents\NUS Y2S1\NOC\Misc\Calhacks\Webcam Images\newMesh.stl'
 
+mode = "B"
+
 def init():
     global screenCam, stlImage, x_dim, newMesh
+
+    fileDirectory = input("Please enter file directory: ") + "/"
+    fileName = input("Please enter file name: ")
+    if not fileName and fileDirectory == "/":
+        filepath = referenceModelPath
+    else:
+      filepath = fileDirectory + fileName
+    print(filepath)
+        
     screenCam = wb.screenRecorder()
-    newMesh = stleditor.stlMesh(path)
+    newMesh = stleditor.stlMesh(filepath)
     # newMesh.plotMesh()
-    newMesh.saveSnapshot(savePath)
+    newMesh.saveSnapshot(savePath, mode)
     x_dim = newMesh.find_x_dim()
     print(f"X dimension = {x_dim}")
     args = {"image":savePath}
@@ -28,13 +41,19 @@ def init():
 
 async def get_new_mesh(mesh, savePath):
     global x_dim
-    os.remove(savePath)
-    mesh.saveSnapshot(savePath)
+    mesh.saveSnapshot(savePath, mode)
     x_dim = mesh.find_x_dim()
     # print(f"X dimension = {x_dim}")
     args = {"image":savePath}
     return cvImage(args)
 
+async def get_new_image(mesh, newPartHeight:int or float, meshPath:str):
+    splitRes = mesh.section(newPartHeight)
+    trimesh.exchange.export.export_mesh(splitRes, meshPath, 'stl')
+    splitMesh = stleditor.stlMesh(meshPath)
+    # newMesh.plotMesh()
+    newImage = await get_new_mesh(splitMesh, savePath)
+    return newImage
 
 
 async def main():
@@ -43,6 +62,7 @@ async def main():
     decrementFlag = False
     highestMatch = {"HuDiff":5, "Height":0}
     bestPartHeights = np.array([],dtype=float)
+
     while True:
         image = screenCam.screenshot()
         # print(image)
@@ -60,56 +80,84 @@ async def main():
             imageMatch = 0
         screencapImage.write_text(300, 400, f"Match = {imageMatch}%")
         
-        if imageMatch > 92:
-            newPartHeight = screencapImage.draw_bounding_box(width=x_dim,visibility=True)
-        else:
-            newPartHeight = screencapImage.draw_bounding_box(width=x_dim,visibility=False)
-       
-        # print(newPartHeight)
-        if newPartHeight:
-            heightDiff = oldPartHeight - newPartHeight  
-            # print(heightDiff) 
-            if decrementFlag: 
-                if heightDiff > 0:
-                    decrementFlag = True
 
-                if (abs(heightDiff) > 0.1 and heightDiff < 1 and imageMatch > 98) or (abs(heightDiff) > 0.1 and heightDiff < 0 and imageMatch > 96):
-                                            
+        if mode == 'B':
+            if imageMatch > 90:
+                newPartHeight = screencapImage.draw_bounding_box(width=x_dim,visibility=True)
+            else:
+                newPartHeight = screencapImage.draw_bounding_box(width=x_dim,visibility=False)
+        
+            # print(newPartHeight)
+            if newPartHeight:
+                heightDiff = oldPartHeight - newPartHeight  
+                # print(heightDiff) 
+                if decrementFlag: 
+                    if heightDiff > 0:
+                        decrementFlag = True
+
+                    if (abs(heightDiff) > 0.1 and heightDiff < 1 and imageMatch > 98) or (abs(heightDiff) > 0.1 and heightDiff < 0 and imageMatch > 96):
+                                                
+                            # Get new sliced STL based on height
+                            print(f"Slicing at height: {newPartHeight}")
+                            stlImage = await get_new_image(newMesh, newPartHeight, meshPath)
+                            res = compare_hu_moments(screencapImage, stlImage)
+                            if res == None or huDiff > res:
+                                newMesh = stleditor.stlMesh(meshPath)
+
+                else:
+                    if heightDiff < 0:
+                        decrementFlag = True
+                    print(abs(heightDiff), imageMatch)
+                    if abs(heightDiff) > 0.3 and imageMatch > 86:
                         # Get new sliced STL based on height
                         print(f"Slicing at height: {newPartHeight}")
-                        splitRes = newMesh.section(newPartHeight)
+                        if abs(heightDiff) > 5:
+                            splitRes = newMesh.section(oldPartHeight - 5)
+                        else:
+                            splitRes = newMesh.section(newPartHeight)
                         trimesh.exchange.export.export_mesh(splitRes, meshPath, 'stl')
-                        splitMesh = stleditor.stlMesh(meshPath)
+                        newMesh = stleditor.stlMesh(meshPath)
                         # newMesh.plotMesh()
-                        stlImage = await get_new_mesh(splitMesh, savePath)
-                        res = compare_hu_moments(screencapImage, stlImage)
-                        if res == None or huDiff > res:
-                            newMesh = stleditor.stlMesh(meshPath)
-
-            else:
-                if heightDiff < 0:
-                    decrementFlag = True
-                if abs(heightDiff) > 0.3 and imageMatch > 92:
-                    # Get new sliced STL based on height
-                    # print(f"Slicing at height: {newPartHeight}")
-                    if abs(heightDiff) > 5:
-                        splitRes = newMesh.section(oldPartHeight - 5)
-                    else:
-                        splitRes = newMesh.section(newPartHeight)
-                    trimesh.exchange.export.export_mesh(splitRes, meshPath, 'stl')
-                    newMesh = stleditor.stlMesh(meshPath)
-                    # newMesh.plotMesh()
-                    stlImage = await get_new_mesh(newMesh, savePath)
-            
-            if huDiff and huDiff < highestMatch["HuDiff"]:
-                highestMatch["HuDiff"] = huDiff
-                highestMatch["Height"] = newPartHeight
-                np.append(bestPartHeights, newPartHeight)
-
-                print("New best part height: %s"%(highestMatch["Height"]))
+                        stlImage = await get_new_mesh(newMesh, savePath)
                 
+                if huDiff and huDiff < highestMatch["HuDiff"]:
+                    highestMatch["HuDiff"] = huDiff
+                    highestMatch["Height"] = newPartHeight
+                    np.append(bestPartHeights, newPartHeight)
+
+                    print("New best part height: %s"%(highestMatch["Height"]))
+
+        elif mode == 'T': 
+            partZ = newMesh.find_z_dim()
+            
+            topImage = await get_new_image(newMesh, partZ + 15, meshPath)
+            bottomImage = await get_new_image(newMesh, partZ - 15, meshPath)
+            res1 = compare_hu_moments(screencapImage, topImage)
+            res2 = compare_hu_moments(screencapImage, bottomImage)
+            if res1 == None or res2 == None:
+                continue
+            imageMatch1 = 100 * (1 -  abs(res1 / 10))
+            imageMatch2 = 100 * (1 -  abs(res2 / 10))
+            print(f"Part height: {partZ}, topHu = {res1}, bottomHu = {res2}")
+            if imageMatch < 99 and imageMatch2 < 99:
+                if (abs(res1) - abs(res2)) < 0.001:
+                    newZ = partZ
+                elif abs(res1) > abs(res2) or (abs(res1) - abs(res2)) < 0.1:
+                    newZ = partZ - 15
+                else:
+                    newZ = partZ + 10
+                print(newZ)
+                splitRes = newMesh.section(newZ)
+                trimesh.exchange.export.export_mesh(splitRes, meshPath, 'stl')
+                newMesh = stleditor.stlMesh(meshPath)
+            else:
+                screencapImage.write_text(300, 500, f"Good Match!")
+            screencapImage.write_text(300, 400, f"Match = {max(imageMatch1, imageMatch2)}%")
+            
+
+
         screencapImage.draw_image(250)
-        time.sleep(0.01)
+        await asyncio.sleep(0.01)
 
 if __name__ == "__main__":
     init()
--- CVRepair/stleditor.py
+++ CVRepair/stleditor.py
@@ -37,7 +37,7 @@ class stlMesh:
             mesh.export(savefilepath)
 
     # Save the figure
-    def saveSnapshot(self, savefilepath:str):
+    def saveSnapshot(self, savefilepath:str, mode:str):
         try:
             # data = self.mesh.scene.save_image(resolution=(1080,1080))
             window_conf = pyglet.gl.Config(double_buffer=True, depth_size=6)
@@ -45,7 +45,10 @@ class stlMesh:
             # scene.DirectionalLight.color = [0,0,255,255]
             points = np.array([self.mesh.centroid]) # Point towards CG of mesh
             points[0, 2] = points[0, 2] - 10
-            rotation = np.array([[1,0,0,0],[0,0,-1,0],[0,1,0,0],[0,0,0,1]]) # x,y,z,magnification rotation matrix
+            if mode == "B":
+                rotation = np.array([[1,0,0,0],[0,0,-1,0],[0,1,0,0],[0,0,0,1]]) # x,y,z,magnification rotation matrix
+            elif mode == "T":
+                rotation = None
             scene.camera_transform = scene.camera.look_at(points=points, rotation=rotation, distance=max(self.mesh.extents)*2, center=None)
             
             data = scene.save_image(resolution=[320, 240], window_conf=window_conf)
@@ -64,21 +67,29 @@ class stlMesh:
         except:
             return 0
     
+    def find_z_dim(self):
+        try:
+            meshExtents = self.mesh.extents
+            print(meshExtents)
+            return abs(meshExtents[1])
+        except:
+            return 0
+    
     def section(self, sectionHeight:int or float):
         splitSTL = trimesh.intersections.slice_mesh_plane(self.mesh, (0,0,-1), (0,0,sectionHeight))
         return splitSTL
 
 
 if __name__ == "__main__":
-    path = r"C:\Users\yonx3\Documents\Crossbow\Random\T51_low_poly.stl"
-    savePath2 = r"C:\Users\yonx3\Documents\Crossbow\Random\t-51_slice4.stl"
-    savePath3 = r"C:\Users\yonx3\Documents\Crossbow\Random\t-51_slice5.stl"
+    path = r"C:\Users\yonx3\Documents\Crossbow\Random\catstretch_voronoi updated.stl"
+    savePath2 = r"C:\Users\yonx3\Documents\Crossbow\Random\catstretch_voronoi slice1.stl"
+    savePath3 = r"C:\Users\yonx3\Documents\Crossbow\Random\catstretch_voronoi slice2.stl"
     newMesh = stlMesh(path)
     #newMesh.plotMesh([255,0,0,255])
     #newMesh.saveMesh(savePath1)
     #newMesh.saveSnapshot(savePath2)
-    splitMesh = newMesh.section(150)
+    splitMesh = newMesh.section(10)
     splitMesh.export(savePath2)
-    splitMesh = newMesh.section(200)
+    splitMesh = newMesh.section(30)
     splitMesh.export(savePath3)
     #splitMesh.show()
